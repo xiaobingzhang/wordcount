ef31a4d70a8f4b2b89452e5cd156a3de0076ca1c
xiaobingzhang <xbzhang911@gmail.com>
2014-03-03 10:06:55 +0800

    more test

--- MapReduceTest/src/meanscore/MeanScoreMapper.java
+++ MapReduceTest/src/meanscore/MeanScoreMapper.java
@@ -1,17 +1,15 @@
 package meanscore;
-
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
 public class MeanScoreMapper extends Mapper<Object, Text, Text, FloatWritable> {
 	@Override
 	protected void map(Object key, Text value, Context context)
 			throws IOException, InterruptedException {
 		String line = value.toString();
+		System.out.println(line);
 		StringTokenizer tokens = new StringTokenizer(line, "\n");
 		while (tokens.hasMoreTokens()) {
 			String tmp = tokens.nextToken();
--- MapReduceTest/src/meanscore/MeanScoreReducer.java
+++ MapReduceTest/src/meanscore/MeanScoreReducer.java
@@ -1,11 +1,8 @@
 package meanscore;
-
 import java.io.IOException;
-
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
 public class MeanScoreReducer extends
 		Reducer<Text, FloatWritable, Text, FloatWritable> {
 	@Override
